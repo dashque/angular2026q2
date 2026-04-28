cc0d3ce5908c42542990e463261f89ae58e8f18a
dashque <zagorkiynahyi@gmail.com>
2026-04-28 18:07:51 +0300

    feat(breadcrumbs): add BreadcrumbsComponent with template, styles, and tests

---
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashq-breadcrumbs',
+  imports: [],
+  templateUrl: './breadcrumbs.component.html',
+  styleUrl: './breadcrumbs.component.scss',
+})
+export class BreadcrumbsComponent {}
