f2267686660a3c0911790e8942998b44fedb5857
dashque <zagorkiynahyi@gmail.com>
2026-04-28 08:51:01 +0300

    feat: add Film interface to model film data

---
+++ src/app/shared/models/film.model.ts
@@ -0,0 +1,10 @@
+export interface Film {
+  id: number;
+  title: string;
+  year: number;
+  genre: string;
+  rating: number;
+  description: string;
+  posterUrl: string;
+  isFavorite: boolean;
+}
