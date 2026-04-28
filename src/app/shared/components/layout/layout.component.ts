cdac207af44dd4f957fe03883f12fd37d650a8f3
dashque <zagorkiynahyi@gmail.com>
2026-04-28 18:08:07 +0300

    feat(catalog): enhance CatalogComponent layout and styling, add style tokens

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'dashq-layout',
-  imports: [HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, BreadcrumbsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
