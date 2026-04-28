6d5637c4748ce9f0191b35d7bd3b9837673d6324
dashque <zagorkiynahyi@gmail.com>
2026-04-28 10:24:03 +0300

    feat(footer): add FooterComponent with template, styles, and tests

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashq-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
