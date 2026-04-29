a7e830d1136ef6ba4019da811a0920c239449c58
dashque <zagorkiynahyi@gmail.com>
2026-04-29 09:20:18 +0300

    chore(package): add jest

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+const { createCjsPreset } = require('jest-preset-angular/presets');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  ...createCjsPreset(),
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/test\\.ts$'],
+};
