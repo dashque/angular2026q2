dc6f1bfdfc9dc74c6c807ff4c398e92c58bf8a69
dashque <zagorkiynahyi@gmail.com>
2026-04-30 18:39:10 +0300

    refactor(jest): move coverage reporters to jest.config and remove from angular.json

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   ...createCjsPreset(),
   clearMocks: true,
   coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'lcov'],
   coverageProvider: 'v8',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
