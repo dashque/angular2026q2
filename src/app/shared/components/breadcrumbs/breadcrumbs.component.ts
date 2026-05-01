ab3adebc014843aaf673149fdb2ba4114bff25ef
dashque <zagorkiynahyi@gmail.com>
2026-05-01 11:53:17 +0300

    refactor(breadcrumbs): implement breadcrumb component with dynamic rendering and styling; add Breadcrumb model for structured data

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,21 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { afterNextRender, Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import type { Breadcrumb } from './models/breadcrumb.model';
 
 @Component({
   selector: 'dashq-breadcrumbs',
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './breadcrumbs.component.html',
   styleUrl: './breadcrumbs.component.scss',
 })
 export class BreadcrumbsComponent {
   //Breadcrumbs — show the user's current location in the application. For example: "Home", "Home > Film Title", "About".
   // Clickable breadcrumb items serve as navigation links.
-
   private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+  public readonly breadcrumbs: Breadcrumb[] = [];
+
+  constructor() {
+    afterNextRender(() => {});
+  }
 }
