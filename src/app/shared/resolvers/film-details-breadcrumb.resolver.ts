9faced5e561fb9b97aa7af01fab47786bc247157
dashque <zagorkiynahyi@gmail.com>
2026-05-01 20:52:13 +0300

    feat(film -details-breadcrumb-resolver): add tests

--- src/app/shared/resolvers/film-details-breadcrumb.resolver.ts
+++ src/app/shared/resolvers/film-details-breadcrumb.resolver.ts
@@ -3,7 +3,6 @@ import { type ResolveFn } from '@angular/router';
 import type { HttpResourceRef } from '@angular/common/http';
 import type { Film } from '../models/film.model';
 
-// TODO add tests
 export const filmDetailsBreadcrumbResolver: ResolveFn<string> = (snapshot: ActivatedRouteSnapshot) => {
   const filmResourceReference = snapshot.data['film'] as HttpResourceRef<Film | null> | undefined;
   const film = filmResourceReference?.value();
