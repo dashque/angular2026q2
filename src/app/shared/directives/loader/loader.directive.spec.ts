4ec4a760566f52cc508397c4a9705dd9a14ee3f5
dashque <zagorkiynahyi@gmail.com>
2026-05-01 09:43:03 +0300

    feat(loader-directive): update unit test for LoaderDirective to use TestBed and improve initialization check

--- src/app/shared/directives/loader/loader.directive.spec.ts
+++ src/app/shared/directives/loader/loader.directive.spec.ts
@@ -1,8 +1,25 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { LoaderDirective } from './loader.directive';
 
+@Component({
+  imports: [LoaderDirective],
+  template: `<div [dashqLoader]="false"><span>content</span></div>`,
+})
+class TestHostComponent {}
+
 describe('LoaderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoaderDirective();
-    expect(directive).toBeTruthy();
+  it('должен инициализироваться', () => {
+    TestBed.configureTestingModule({
+      imports: [TestHostComponent],
+    });
+    const fixture = TestBed.createComponent(TestHostComponent);
+
+    fixture.detectChanges();
+    const directiveInstance = fixture.debugElement.query(By.directive(LoaderDirective)).injector.get(LoaderDirective);
+
+    expect(directiveInstance).toBeTruthy();
   });
 });
