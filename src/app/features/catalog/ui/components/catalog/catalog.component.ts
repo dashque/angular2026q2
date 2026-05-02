d823a63c1371d57982b1359a559920c7895b320d
dashque <zagorkiynahyi@gmail.com>
2026-05-02 10:16:38 +0300

    refactor(catalog): replace FilmRepositoryService with FilmListFacade for improved state management

--- src/app/features/catalog/ui/components/catalog/catalog.component.ts
+++ src/app/features/catalog/ui/components/catalog/catalog.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FilmRepositoryService } from '../../../../../shared/services/film-repository/services/film-repository.service';
 import { FilmCardComponent } from './film-card/film-card.component';
 import { Router } from '@angular/router';
 import { AutofocusDirective } from '../../directives/autofocus/autofocus.directive';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoaderDirective } from '../../../../../shared/directives/loader/loader.directive';
+import { FilmListFacade } from '../../../../../shared/facades/film-list/film-list.facade';
 
 @Component({
   selector: 'dashq-catalog',
@@ -13,11 +13,11 @@ import { LoaderDirective } from '../../../../../shared/directives/loader/loader.
   styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
-  private readonly filmRepositoryService = inject(FilmRepositoryService);
+  private readonly filmListFacade = inject(FilmListFacade);
   private readonly router = inject(Router);
-  public readonly isLoading = this.filmRepositoryService.isLoading;
-  public readonly filmList = this.filmRepositoryService.filmList;
-  public readonly searchControl = this.filmRepositoryService.searchForm.controls.searchField;
+  public readonly isLoading = this.filmListFacade.isLoading;
+  public readonly filmList = this.filmListFacade.filmList;
+  public readonly searchControl = this.filmListFacade.searchForm.searchField;
 
   public onCardClick(filmId: number) {
     void this.router.navigate(['details', filmId]);
